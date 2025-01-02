42b89593c520578b597e5e47f97de8bbb37503d8
chachacollins <collinschacha@hotmail.com>
2025-01-02 03:45:27 +0300

    created a basic repl skeleton

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
-const ut = @import("utils.zig");
+const repl = @import("repl.zig");
 
 pub fn main() !void {
-    const char = 'A';
-    const not_printable = &[_]u8{char};
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-    try stdout.print("Hello {s} not_printable{s}\n", .{ not_printable, not_printable });
-    try bw.flush(); // don't forget to flush!
-    if (std.mem.eql(u8, "{", "{")) {
-        std.debug.print("thes two are equal", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const ds = gpa.deinit();
+        if (ds == .leak) {
+            std.log.err("memory leak", .{});
+        }
     }
+    const allocator = gpa.allocator();
+    try repl.start(allocator);
 }
---
+++ src/repl.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const lexer = @import("lexer.zig");
+const token = @import("token.zig");
+
+pub fn start(Allocator: std.mem.Allocator) !void {
+    const prompt = ">>";
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    // var input = std.ArrayList([]const u8).init(Allocator);
+    // defer input.deinit();
+    const stdout = bw.writer();
+    try stdout.print("Aargh me hearty! Welcome to tha Jolly Roger\n", .{});
+    while (true) {
+        try stdout.print("{s}", .{prompt});
+        try bw.flush(); // don't forget to flush!
+
+        const stdin_file = std.io.getStdIn().reader();
+        var br = std.io.bufferedReader(stdin_file);
+        const stdin = br.reader();
+        const line = try stdin.readUntilDelimiterOrEofAlloc(Allocator, '\n', 4096) orelse "";
+        var l = lexer.Lexer.init(line);
+        for (line.len) |_| {
+            const tok = try l.nextToken();
+            std.debug.print("token type:{} token.literal {s}\n", .{ tok.Type, tok.Literal });
+            if (tok.Type == token.TokenType.EOF) {
+                break;
+            }
+        }
+        defer Allocator.free(line);
+    }
+}
